3fe68f07f56165092ff083a3834efd7fa6f7eeed
Eunice Heymann <heymann.eunice@gmail.com>
2016-11-02 11:41:31 -0500

    Added animation for gameplay, working on style and cleaning up code.

--- js/simplesimon.js
+++ js/simplesimon.js
@@ -14,21 +14,28 @@ game = {
     }
 };
 
-//play sound when corresponding color is called
 
+
+
+//play sound when corresponding color is called
 function sound(color) {
+    console.log(color);
     switch(color) {
         case'#green':
             game.sound.green.play();
+            $("#green").addClass("animated pulse");
             break;
         case '#blue':
             game.sound.blue.play();
+            $("#blue").addClass("animated pulse");
             break;
         case '#red':
             game.sound.red.play();
+            $("#red").addClass("animated pulse");
             break;
         case '#yellow':
             game.sound.yellow.play();
+            $("#yellow").addClass("animated pulse");
             break;
     }
 }
@@ -38,7 +45,7 @@ function sound(color) {
 function simonMoves() {
     var i = 0;
     var moves = setInterval(function(){
-        playGame(game.currentGame[i]);
+        startGame(game.currentGame[i]);
         i++;
         if (i >= game.currentGame.length) {
             clearInterval(moves);
@@ -47,17 +54,12 @@ function simonMoves() {
 
     playerMoves();
 }
-
-//sound is associated with color played by Simon and player
-
-function playGame(play) {
-    $(play).addClass("hover");
+//game play is initiated
+function startGame(play) {
     sound(play);
     setTimeout(function(){
-        $(play).removeClass("hover");
-    }, 400);
+    }, 600);
 }
-
 
 //creates game.player array to annotate player's moves
 
@@ -66,23 +68,24 @@ function playerMoves() {
 }
 
 
-//created variable to associate array from playerMoves to an ID and pushed the array to playerTurn
+//created variable to associate array from playerMoves to an ID and pushed the array to playersTurn
 function addToPlayer(id) {
     var play = "#" + id;
     console.log(play);
     game.player.push(play);
-    playerTurn(play);
+    playersTurn(play);
 }
 
 //created function with if/else statements to determine whether player's moves were the same as Simon's
-function playerTurn(x) {
+function playersTurn(x) {
+
     if (game.player[game.player.length - 1] !== game.currentGame[game.player.length - 1]) {
         alert('Wrong move! Try again!');
         simonMoves();
 
     } else {
-        console.log("Good Move!");
         sound(x);
+        console.log("Good Move!");
         var check = game.player.length === game.currentGame.length;
         if (check) {
             if(game.count == 35){
@@ -104,13 +107,15 @@ function generateMove(){
 
 function addCount() {
     game.count++;
-    $('#countLevel').addClass();
+    $("#countLevel").addClass("animated fadeOutDown");
+
     setTimeout(function(){
-        $('#countLevel').removeClass().html(game.count).addClass();
+        $("#countLevel").removeClass("fadeOutDown").html(game.count).addClass("fadeInDown");
     }, 200);
 
     generateMove();
 }
+
 
 function nextLevel() {
     addCount();
